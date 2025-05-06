415d6b585c1bb2f4af78e0346d7c0db783aa51af
fit-s-u-m <nehemiahmaths@gmail.com>
2025-05-07 01:07:43 +0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Abe')
+    .setDescription(' AI amahric assistant')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT ?? 80);
 }
