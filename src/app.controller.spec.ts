efca9b092383d7baf552d5eef9c6884c65d4b2ff
fit-s-u-m <nehemiahmaths@gmail.com>
2025-05-18 00:44:55 +0300

    write a simple test and added the right dependacy

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AudioService } from './11Labs/audio.service';
+import { ElevenLabsService } from './11Labs/elevenLabs.service';
+import { GeminiService } from './Gemini/Gemini.service';
+import { BroadcastGateway } from './ws/ws.gateway';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +12,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, ElevenLabsService, GeminiService, AudioService, BroadcastGateway],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -16,7 +20,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.hello()).toBe('Hello World!');
+      expect(appController.hello()).toBe('Hello from abe');
     });
   });
 });
